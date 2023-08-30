dd0b74ba293efc85c4718ce1e1b1281e9740ab47
Attila Oláh <attilaolah@gmail.com>
2023-08-30 09:13:53 +0200

    Add some comments.

--- py/poly/int_p_test.py
+++ py/poly/int_p_test.py
@@ -11,19 +11,19 @@ class TestIntP(unittest.TestCase):
         """Tests the add method."""
         self.assertEqual(
             repr(int_p.IntP(
-                (2, (1,)),
+                (2, (1,)),  # 2x
             ) + int_p.IntP(
-                (2, (1,)),
+                (2, (1,)),  # 2x
             )),
             '4x'
         )
         self.assertEqual(
             repr(int_p.IntP(
-                (1, (0, 1)),
-                (1, (1, 0)),
-                (2, (1, 1)),
+                (1, (0, 1)),  # y
+                (1, (1, 0)),  # x
+                (2, (1, 1)),  # 2xy
             ) + int_p.IntP(
-                (2, (1, 1)),
+                (2, (1, 1)),  # 2xy
             )),
             '4xy + x + y'
         )
